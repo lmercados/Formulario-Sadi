3a94cc2b1c9b9b8749ab08f7ff6f753d0ab5a5df
lmercados <luisalbertomercado15@hotmail.com>
2020-05-22 22:53:13 -0400

    Formulario Completado

--- src/views/Retirar-Transformador.vue
+++ src/views/Retirar-Transformador.vue
@@ -8,7 +8,7 @@
             <v-card-subtitle class="ml-3 red--text font-weight-light title">Formulario de Retiro</v-card-subtitle>
             <v-row class="ma-6">
               <v-col cols="12" xs12 md="4">
-                <datePicker title="Fecha de Reposición"></datePicker>
+                <datePicker picker="Fecha de Reposición"></datePicker>
               </v-col>
               <v-col
                 cols="12"
@@ -17,7 +17,7 @@
                 v-for="(text,indextr) in texto_retirado"
                 :key="text.model[indextr]"
               >
-                <v-text-field ml-3 v-model="text.model[indextr]" :label="text.name" required></v-text-field>
+                <v-text-field ml-3 v-model="valor" :label="text.name" required></v-text-field>
               </v-col>
               <v-col
                 cols="12"
@@ -35,7 +35,34 @@
                 ></v-select>
               </v-col>
             </v-row>
+            <v-row>
+              <v-dialog v-model="dialog_voltajes" max-width="450" color>
+                <v-card class="blue-grey lighten-5">
+                  <v-card-title>Agregar Posiciones</v-card-title>
 
+                  <v-col
+                    cols="12"
+                    v-for="(texto_p,indext_posicion) in texto_posicion"
+                    :key="texto_p.name[indext_posicion]"
+                  >
+                    <v-text-field
+                      solo
+                      class="mx-5"
+                      v-model="texto_p.model[indext_posicion]"
+                      :label="texto_p.name"
+                      required
+                    ></v-text-field>
+                  </v-col>
+
+                  <div class="text-center">
+                    <v-btn class="mb-5" rounded color="success" dark>Agregar</v-btn>
+                  </div>
+                </v-card>
+              </v-dialog>
+              <v-col xs12>
+                <v-btn @click.stop="dialog_voltajes = true" block color="warning">Agregar Voltajes</v-btn>
+              </v-col>
+            </v-row>
             <v-divider></v-divider>
 
             <v-card-subtitle class="ml-3 red--text font-weight-light title">Formulario de Instalado</v-card-subtitle>
@@ -55,7 +82,39 @@
                 ></v-text-field>
               </v-col>
             </v-row>
-
+            <v-row>
+              <v-dialog v-model="dialog_materiales" max-width="450" color>
+                <v-card class="blue-grey lighten-5">
+                  <v-card-title>Agregar Materiales</v-card-title>
+                  <v-col cols="12">
+                    <v-select
+                      ml-3
+                      :items="data_materiales"
+                      v-model="material"
+                      label="Seleccione el Material"
+                      required
+                    ></v-select>
+                    <p>{{material}}</p>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field solo label="Cantidad" v-model="cantidad_material" required></v-text-field>
+                  </v-col>
+                  <div class="text-center">
+                    <v-btn class="mb-5" rounded color="success" @click="agregarMaterial" dark>Agregar</v-btn>
+                  </div>
+                </v-card>
+              </v-dialog>
+              <v-col xs12>
+                <v-btn
+                  @click.stop="dialog_materiales = true"
+                  block
+                  color="warning"
+                >Agregar Materiales</v-btn>
+              </v-col>
+            </v-row>
+            <v-row class="mx-5">
+              <v-data-table :headers="headers" :items="materiales_utilizados" hide-default-footer></v-data-table>
+            </v-row>
             <v-row>
               <v-col xs12>
                 <v-btn block color="success" dark>Enviar Datos</v-btn>
@@ -82,8 +141,28 @@ export default {
     datePicker
   },
   data: () => ({
+    valor: "",
+    dialog_voltajes: false,
+    dialog_materiales: false,
     select: null,
     valid: true,
+    data_materiales: ["Cut-Out", "Apartarrayos", "Pica Pat"],
+    material: "",
+    cantidad_material: "",
+    materiales_utilizados: [],
+    headers: [
+          {
+            text: 'Material',
+            align: 'start',
+            sortable: false,
+            value: 'material',
+          },
+          {
+            text: 'Cantidad',
+            align: 'start',
+            sortable: false,
+            value: 'cantidad',
+          }],
     combo_retirado: [
       { name: "Poste", select: { sPoste: "" }, data: [1, 2, 3] },
       { name: "Causa", select: { sCausa: "" }, data: [4, 5, 6] },
@@ -117,12 +196,14 @@ export default {
       { name: "Dirección", model: { tDireccion: "" } },
       { name: "Impedancia", model: { tImpedancia: "" } },
       { name: "Referencia", model: { tRefrencia: "" } },
-      { name: "Orden", model: { tOrden: "" } },
-      { name: "Volateje Posicion 1", model: { tPosicion1: "" } },
-      { name: "Voltaje Posicion 2", model: { tPosicion2: "" } },
-      { name: "Voltaje Posicion 3", model: { tPosicion3: "" } },
-      { name: "Voltaje Posicion 4", model: { tPosicion4: "" } },
-      { name: "Voltaje Posicion 5", model: { tPosicion5: "" } }
+      { name: "Orden", model: { tOrden: "" } }
+    ],
+    texto_posicion: [
+      { name: "Posición 1", model: { tPosicion_1: "" } },
+      { name: "Posición 2", model: { tPosicion_2: "" } },
+      { name: "Posición 3", model: { tPosicion_3: "" } },
+      { name: "Posición 4", model: { tPosicion_4: "" } },
+      { name: "Posición 5", model: { tPosicion_5: "" } }
     ],
     texto_instalado: [
       { name: "Serie", model: { tSeriei: "" } },
@@ -146,6 +227,10 @@ export default {
     },
     resetValidation() {
       this.$refs.form.resetValidation();
+    },
+    agregarMaterial() {
+      this.materiales_utilizados.push({material:this.material,cantidad:this.cantidad_material});
+      
     }
   },
   mounted() {}
