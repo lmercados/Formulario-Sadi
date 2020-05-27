6f186510d5cc0e0e84192ae0839b229a02b3941d
lmercados <luisalbertomercado15@hotmail.com>
2020-05-26 23:05:26 -0400

    Formulario con Sleppers

--- src/views/Retirar-Transformador.vue
+++ src/views/Retirar-Transformador.vue
@@ -2,131 +2,177 @@
   <div>
     <v-row>
       <v-col xs="12" md="8" offset-md="2" class="mt-10">
-        <v-card>
-          <h2 class="text-center white--text error subtitle-3">Proceso de Intervención</h2>
-          <v-form style="border: 2px solid grey;padding: 10px;border-radius: 15px;" class="ma-6">
-            <v-card-subtitle class="ml-3 red--text font-weight-light title">Formulario de Retiro</v-card-subtitle>
-            <v-row class="ma-6">
-              <v-col cols="12" xs12 md="4">
-                <datePicker picker="Fecha de Reposición"></datePicker>
-              </v-col>
-              <v-col
-                cols="12"
-                xs12
-                md="4"
-                v-for="(text,indextr) in texto_retirado"
-                :key="text.model[indextr]"
-              >
-                <v-text-field ml-3 v-model="valor" :label="text.name" required></v-text-field>
-              </v-col>
-              <v-col
-                cols="12"
-                xs12
-                md="4"
-                v-for="(item,indexcr) in combo_retirado"
-                :key="item.select[indexcr]"
-              >
-                <v-select
-                  ml-3
-                  :items="item.data"
-                  v-model="item.select[indexcr]"
-                  :label="item.name"
-                  required
-                ></v-select>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-dialog v-model="dialog_voltajes" max-width="450" color>
-                <v-card class="blue-grey lighten-5">
-                  <v-card-title>Agregar Posiciones</v-card-title>
-
+        <v-stepper v-model="e6" vertical>
+          <v-form>
+            <v-card-title class="blue--text">Proceso de Intervención</v-card-title>
+            <v-stepper-step :complete="e6 > 1" step="1">Datos Generales</v-stepper-step>
+            <v-stepper-content step="1">
+              <v-card class="mb-12">
+                <v-row class="ma-6">
+                  <v-col cols="12" xs12 md="4">
+                    <datePicker solo picker="Fecha de Reposición"></datePicker>
+                  </v-col>
                   <v-col
                     cols="12"
-                    v-for="(texto_p,indext_posicion) in texto_posicion"
-                    :key="texto_p.name[indext_posicion]"
+                    xs12
+                    md="4"
+                    v-for="(datos_g,index) in datos_generales"
+                    :key="index"
                   >
                     <v-text-field
+                      v-if="datos_g.tipo==1"
                       solo
-                      class="mx-5"
-                      v-model="texto_p.model[indext_posicion]"
-                      :label="texto_p.name"
+                      ml-3
+                      v-model="datos_g.model[index]"
+                      :label="datos_g.name"
+                      required
+                    ></v-text-field>
+                    <v-select
+                      v-else
+                      ml-3
+                      :items="datos_g.data"
+                      v-model="datos_g.select[index]"
+                      :label="datos_g.name"
+                      required
+                      solo
+                    ></v-select>
+                  </v-col>
+                </v-row>
+              </v-card>
+              <v-btn class="mx-2" color="primary" @click="e6 = 2">Continue</v-btn>
+            </v-stepper-content>
+            <v-stepper-step :complete="e6 > 2" step="2">Transformador a Retirar</v-stepper-step>
+
+            <v-stepper-content step="2">
+              <v-card class="mb-12">
+                <v-row class="ma-6">
+                  <v-col cols="12" xs12 md="4" v-for="(tr_r,indexr) in tr_retirar" :key="indexr">
+                    <v-text-field
+                      v-if="tr_r.tipo==1"
+                      solo
+                      ml-3
+                      v-model="tr_r.model[indexr]"
+                      :label="tr_r.name"
+                      required
+                    ></v-text-field>
+                    <v-select
+                      v-else
+                      ml-3
+                      :items="tr_r.data"
+                      v-model="tr_r.select[indexr]"
+                      :label="tr_r.name"
+                      required
+                      solo
+                    ></v-select>
+                  </v-col>
+                </v-row>
+                <v-dialog v-model="dialog_voltajes" max-width="450" color>
+                  <v-card class="blue-grey lighten-5">
+                    <v-card-title>Agregar Posiciones</v-card-title>
+
+                    <v-col
+                      cols="12"
+                      v-for="(texto_p,indext_posicion) in texto_posicion"
+                      :key="texto_p.name[indext_posicion]"
+                    >
+                      <v-text-field
+                        solo
+                        class="mx-5"
+                        v-model="texto_p.model[indext_posicion]"
+                        :label="texto_p.name"
+                        required
+                      ></v-text-field>
+                    </v-col>
+
+                    <div class="text-center">
+                      <v-btn class="mb-5" rounded color="success" dark>Agregar</v-btn>
+                    </div>
+                  </v-card>
+                </v-dialog>
+                <v-col xs12>
+                  <v-btn @click.stop="dialog_voltajes = true" color="error" large>Agregar Voltajes</v-btn>
+                </v-col>
+              </v-card>
+              <v-btn class="mx-2" color="primary" @click="e6 = 3">Continue</v-btn>
+              <v-btn class="mx-2" text color="warning" @click="e6 = 1">Back</v-btn>
+            </v-stepper-content>
+
+            <v-stepper-step :complete="e6 > 3" step="3">Transformador a Instalar</v-stepper-step>
+
+            <v-stepper-content step="3">
+              <v-card class="mb-12">
+                <v-row class="ma-6">
+                  <v-col cols="12" xs12 md="4" v-for="(tr_i,indexi) in tr_instalar" :key="indexi">
+                    <v-text-field
+                      solo
+                      :disabled="tr_i.disabled"
+                      ml-3
+                      v-model="tr_i.model[indexi]"
+                      :label="tr_i.name"
                       required
                     ></v-text-field>
                   </v-col>
-
-                  <div class="text-center">
-                    <v-btn class="mb-5" rounded color="success" dark>Agregar</v-btn>
-                  </div>
-                </v-card>
-              </v-dialog>
-              <v-col xs12>
-                <v-btn @click.stop="dialog_voltajes = true" block color="warning">Agregar Voltajes</v-btn>
-              </v-col>
-            </v-row>
-            <v-divider></v-divider>
-
-            <v-card-subtitle class="ml-3 red--text font-weight-light title">Formulario de Instalado</v-card-subtitle>
-            <v-row class="ma-6">
-              <v-col
-                cols="12"
-                xs12
-                md="4"
-                v-for="(texto_instalado,indexti) in texto_instalado"
-                :key="texto_instalado.model[indexti]"
-              >
-                <v-text-field
-                  ml-3
-                  v-model="texto_instalado.model[indexti]"
-                  :label="texto_instalado.name"
-                  required
-                ></v-text-field>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-dialog v-model="dialog_materiales" max-width="450" color>
-                <v-card class="blue-grey lighten-5">
-                  <v-card-title>Agregar Materiales</v-card-title>
-                  <v-col cols="12">
-                    <v-select
-                      ml-3
-                      :items="data_materiales"
-                      v-model="material"
-                      label="Seleccione el Material"
-                      required
-                    ></v-select>
-                    <p>{{material}}</p>
+                  <v-dialog v-model="dialog_materiales" max-width="450" color>
+                    <v-card class="blue-grey lighten-5">
+                      <v-card-title>Agregar Materiales</v-card-title>
+                      <v-col cols="12">
+                        <v-select
+                          solo
+                          :items="data_materiales"
+                          v-model="material"
+                          label="Seleccione el Material"
+                          required
+                        ></v-select>
+                      </v-col>
+                      <v-col cols="12">
+                        <v-text-field solo label="Cantidad" v-model="cantidad_material" required></v-text-field>
+                      </v-col>
+                      <div class="text-center">
+                        <v-btn
+                          class="mb-5"
+                          rounded
+                          color="success"
+                          @click="agregarMaterial"
+                          dark
+                        >Agregar</v-btn>
+                      </div>
+                    </v-card>
+                  </v-dialog>
+                  <v-col xs12>
+                    <v-btn @click.stop="dialog_materiales = true" color="error">Agregar Materiales</v-btn>
                   </v-col>
-                  <v-col cols="12">
-                    <v-text-field solo label="Cantidad" v-model="cantidad_material" required></v-text-field>
+                </v-row>
+
+                <v-row>
+                  <v-col xs12 md="12">
+                    <v-btn text class="mx-2" color="warning" @click="e6 = 2">Back</v-btn>
                   </v-col>
-                  <div class="text-center">
-                    <v-btn class="mb-5" rounded color="success" @click="agregarMaterial" dark>Agregar</v-btn>
-                  </div>
+                </v-row>
+              </v-card>
+              <v-row class="mx-5">
+                <v-card>
+                  <v-card-title>Materiales Utilizados</v-card-title>
+
+                  <v-data-table
+                    :headers="headers"
+                    :items="materiales_utilizados"
+                    hide-default-footer
+                  ></v-data-table>
                 </v-card>
-              </v-dialog>
-              <v-col xs12>
-                <v-btn
-                  @click.stop="dialog_materiales = true"
-                  block
-                  color="warning"
-                >Agregar Materiales</v-btn>
-              </v-col>
-            </v-row>
-            <v-row class="mx-5">
-              <v-data-table :headers="headers" :items="materiales_utilizados" hide-default-footer></v-data-table>
-            </v-row>
-            <v-row>
-              <v-col xs12>
-                <v-btn block color="success" dark>Enviar Datos</v-btn>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col xs12>
-                <v-btn block color="error" dark>Limpiar Datos</v-btn>
-              </v-col>
-            </v-row>
+              </v-row>
+              <v-row>
+                <v-col xs12>
+                  <v-btn block color="success" dark>Enviar Datos</v-btn>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col xs12>
+                  <v-btn block color="error" dark>Limpiar Datos</v-btn>
+                </v-col>
+              </v-row>
+            </v-stepper-content>
           </v-form>
-        </v-card>
+        </v-stepper>
       </v-col>
     </v-row>
   </div>
@@ -141,6 +187,7 @@ export default {
     datePicker
   },
   data: () => ({
+    e6: 1,
     valor: "",
     dialog_voltajes: false,
     dialog_materiales: false,
@@ -151,31 +198,42 @@ export default {
     cantidad_material: "",
     materiales_utilizados: [],
     headers: [
-          {
-            text: 'Material',
-            align: 'start',
-            sortable: false,
-            value: 'material',
-          },
-          {
-            text: 'Cantidad',
-            align: 'start',
-            sortable: false,
-            value: 'cantidad',
-          }],
-    combo_retirado: [
-      { name: "Poste", select: { sPoste: "" }, data: [1, 2, 3] },
-      { name: "Causa", select: { sCausa: "" }, data: [4, 5, 6] },
-      { name: "Fase", select: { sCausa: "" }, data: [7, 8, 9] },
-      { name: "Viviendas", select: { sViviendas: "" }, data: [10, 11, 12] },
-      { name: "Fabricante", select: { sFabricante: "" }, data: [13, 14, 15] },
-      { name: "Capacidad", select: { sCapacidad: "" }, data: [16, 17, 18] },
+      {
+        text: "Material",
+        align: "start",
+        sortable: false,
+        value: "material"
+      },
+      {
+        text: "Cantidad",
+        align: "start",
+        sortable: false,
+        value: "cantidad"
+      }
+    ],
+    datos_generales: [
+      { name: "Matrícula CT", model: { tCt: "" }, tipo: 1 },
+      { name: "Orden", model: { tOrden: "" }, tipo: 1 },
+      { name: "Aviso", model: { tAviso: "" }, tipo: 1 },
+      { name: "Dirección", model: { tDireccion: "" }, tipo: 1 },
+      { name: "Referencia", model: { tRefrencia: "" }, tipo: 1 },
       { name: "Circuito", select: { Scircuito: "" }, data: [19, 20, 21] },
+      { name: "Poste", select: { sPoste: "" }, data: [1, 2, 3] },
+      { name: "Viviendas", select: { sViviendas: "" }, data: [10, 11, 12] },
+      { name: "Causa", select: { sCausa: "" }, data: [4, 5, 6] }
+    ],
+    tr_retirar: [
+      { name: "Serie", model: { tSerieR: "" }, tipo: 1 },
+      { name: "Impedancia", model: { tImpedanciaR: "" }, tipo: 1 },
       {
         name: "Tipo de Transformador",
         select: { Stipotransfomador: "" },
         data: [22, 23, 24]
       },
+      { name: "Fase", select: { sCausa: "" }, data: [7, 8, 9] },
+      { name: "Fabricante", select: { sFabricante: "" }, data: [13, 14, 15] },
+      { name: "Capacidad", select: { sCapacidad: "" }, data: [16, 17, 18] },
+
       { name: "Tipo de Bush", select: { tBush: "" }, data: [25, 26, 27] },
       { name: "Regulación", select: { sRegulacion: "" }, data: [28, 29, 30] },
       {
@@ -189,15 +247,7 @@ export default {
         data: [34, 35, 36]
       }
     ],
-    texto_retirado: [
-      { name: "Serie", model: { tSerier: "" } },
-      { name: "Etiqueta Instalada", model: { tCt: "" } },
-      { name: "Aviso", model: { tAviso: "" } },
-      { name: "Dirección", model: { tDireccion: "" } },
-      { name: "Impedancia", model: { tImpedancia: "" } },
-      { name: "Referencia", model: { tRefrencia: "" } },
-      { name: "Orden", model: { tOrden: "" } }
-    ],
+
     texto_posicion: [
       { name: "Posición 1", model: { tPosicion_1: "" } },
       { name: "Posición 2", model: { tPosicion_2: "" } },
@@ -205,14 +255,18 @@ export default {
       { name: "Posición 4", model: { tPosicion_4: "" } },
       { name: "Posición 5", model: { tPosicion_5: "" } }
     ],
-    texto_instalado: [
+    tr_instalar: [
       { name: "Serie", model: { tSeriei: "" } },
-      { name: "Fabricante", model: { tFabricantei: "" } },
-      { name: "Capacidad", model: { tCapacidadi: "" } },
-      { name: "Tensión Primaria", model: { tVoltajePrimarioi: "" } },
-      { name: "Fases", model: { sFasesi: "" } },
-      { name: "Voltaje Fase A", model: { tVoltajeFaseA: "" } },
-      { name: "Voltaje Fase B", model: { tVoltajeFaseB: "" } },
+      { name: "Fabricante", model: { tFabricantei: "" }, disabled: true },
+      { name: "Capacidad", model: { tCapacidadi: "" }, disabled: true },
+      {
+        name: "Tensión Primaria",
+        model: { tVoltajePrimarioi: "" },
+        disabled: true
+      },
+      { name: "Fases", model: { sFasesi: "" }, disabled: true },
+      { name: "Voltaje Fase A", model: { tVoltajeFaseA: "" }, disabled: false },
+      { name: "Voltaje Fase B", model: { tVoltajeFaseB: "" }, disabled: false },
       { name: "Voltaje Fase C", model: { tVoltajeFaseC: "" } }
     ]
   }),
@@ -229,8 +283,10 @@ export default {
       this.$refs.form.resetValidation();
     },
     agregarMaterial() {
-      this.materiales_utilizados.push({material:this.material,cantidad:this.cantidad_material});
-      
+      this.materiales_utilizados.push({
+        material: this.material,
+        cantidad: this.cantidad_material
+      });
     }
   },
   mounted() {}
